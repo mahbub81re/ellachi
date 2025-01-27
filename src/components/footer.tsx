c024e1b4ccf0d2207bd60a14aec55c640adb3b2f
Mahbub Mohsin <mollahmahbub81@gmail.com>
2025-01-27 21:01:42 +0600

    update 9

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,97 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
+import { AiOutlineDown } from 'react-icons/ai'
+import Logo from './logo'
+import { FaFacebookSquare, FaInstagram, FaTwitter, FaYoutube } from 'react-icons/fa'
+import Link from 'next/link'
 
 export default function Footer() {
+  const [about,setAbout] = useState(true);
+  const [info,setInfo] = useState(false);
+  const [ser,setSer] = useState(false);
   return (
-    <div>Footer</div>
+    <div className='p-5 flex flex-col pb-20 bg-indigo-100'>
+      {/* About us  */}
+       <div className='flex flex-col'>
+          <div className=' flex flex-row justify-between border-b-2 pb-2  ' onClick={()=>setAbout(!about)} >
+             <div className=' text-2xl'>About us</div> 
+             <div><AiOutlineDown  size={24}/></div>
+            
+          </div>
+         
+         
+         
+          {about &&<div className='pt-2'>
+             <Logo/>
+             <div className='p-2'>
+             Welcome to our online store! We are a team of passionate individuals who are dedicated to providing our customers with high-quality products and excellent service.
+             </div>
+             <div className='flex flex-row justify-start'>
+               <div  className=' m-2'><FaFacebookSquare  size={24}/></div>
+               <div  className=' m-2'>  <FaTwitter  size={24}/></div>
+               <div  className=' m-2'>   <FaInstagram  size={24}/></div>
+               <div  className=' m-2'>  <FaYoutube  size={24}/> </div>
+
+              
+             
+             </div>
+          </div>
+}
+       </div>
+
+
+
+       <div className='flex flex-col mt-2'>
+          <div className=' flex flex-row justify-between border-b-2 pb-2 ' onClick={()=>setInfo(!info)} >
+             <div className=' text-2xl'>Information</div> 
+             <div><AiOutlineDown  size={24}/></div>
+            
+          </div>
+
+          {info && <div className='flex- flex-col m-3 text-md'>
+           
+            <div className=' hover:pl-2'> <Link href='/'> About Us</Link></div>
+            <div className=' hover:pl-2'> <Link href='/'> Delivery Information</Link></div>
+            <div className=' hover:pl-2'> <Link href='/'> Privacy Policy</Link></div>
+            <div className=' hover:pl-2'> <Link href='/'> Terms & Conditions</Link></div>
+            <div className=' hover:pl-2'> <Link href='/'> Contact Us</Link></div>
+            <div className=' hover:pl-2'> <Link href='/'> Returns</Link></div>
+          </div>}
+        </div>
+
+
+
+        <div className='flex flex-col'>
+          <div className=' flex flex-row justify-between border-b-2 pb-2 ' onClick={()=>setSer(!ser)} >
+             <div className=' text-2xl'>Customer Service</div> 
+             <div><AiOutlineDown  size={24}/></div>
+            
+          </div>
+
+        {ser &&<div className='flex- flex-col m-3 text-md'>
+           
+            <div className=' hover:pl-2'> <Link href='/'> Site Map</Link></div>
+            <div className=' hover:pl-2'> <Link href='/'> Wish List</Link></div>
+            <div className=' hover:pl-2'> <Link href='/'> Brands</Link></div>
+            <div className=' hover:pl-2'> <Link href='/'> Gift Certificates</Link></div>
+            <div className=' hover:pl-2'> <Link href='/'> Affiliate</Link></div>
+            <div className=' hover:pl-2'> <Link href='/'> Specials</Link></div>
+            <div className=' hover:pl-2'> <Link href='/'> Order Tracking</Link></div>
+            
+          </div>
+        }
+          
+        </div>
+
+
+        
+
+
+
+
+
+
+
+    </div>
   )
 }
--- src/components/homeproducts.tsx
+++ src/components/homeproducts.tsx
@@ -10,7 +10,7 @@ export default function HomeProducts() {
           </div>
 
           <div className='flex flex-row justify-center p-4'>
-          <div className="grid max-sm:grid-cols-2 max-md:grid-cols-2 max-xl:grid-cols-3 gap-2">
+          <div className="grid max-sm:grid-cols-2 max-md:grid-cols-2 max-xl:grid-cols-4 grid-cols-5 gap-3">
 
               
 
@@ -52,7 +52,7 @@ export default function HomeProducts() {
     <Card
       className="max-w-sm"
       imgAlt="Apple Watch Series 7 in colors pink, silver, and black"
-      imgSrc="/slider2/1.png"
+      imgSrc="/slider2/2.png"
     >
       <a href="#">
         <h5 className="text-sm font-semibold tracking-tight text-gray-900 dark:text-white">
@@ -82,6 +82,107 @@ export default function HomeProducts() {
     </Card>
 
 
+    <Card
+      className="max-w-sm"
+      imgAlt="Apple Watch Series 7 in colors pink, silver, and black"
+      imgSrc="/slider2/3.png"
+    >
+      <a href="#">
+        <h5 className="text-sm font-semibold tracking-tight text-gray-900 dark:text-white">
+          Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport
+        </h5>
+      </a>
+      <div className="mb-1 mt-1 flex flex-row items-center">
+       
+       <span className="text-xl font-bold  dark:text-white pr-2 text-red-500"><del>$599</del></span>
+       <span className="text-xl font-bold text-green-900 dark:text-white">$400</span>
+      </div>
+      <div className="flex flex-col ">
+       
+        <a
+          href="#"
+          className="rounded-lg mb-1 bg-cyan-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-cyan-800 focus:outline-none focus:ring-4 focus:ring-cyan-300 dark:bg-cyan-600 dark:hover:bg-cyan-700 dark:focus:ring-cyan-800"
+        >
+          Add to cart
+        </a>
+        <a
+          href="#"
+          className="rounded-lg bg-pink-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-cyan-800 focus:outline-none focus:ring-4 focus:ring-cyan-300 dark:bg-cyan-600 dark:hover:bg-cyan-700 dark:focus:ring-cyan-800"
+        >
+        Buy Now
+        </a>
+      </div>
+    </Card>
+
+
+
+
+    <Card
+      className="max-w-sm"
+      imgAlt="Apple Watch Series 7 in colors pink, silver, and black"
+      imgSrc="/slider2/4.png"
+    >
+      <a href="#">
+        <h5 className="text-sm font-semibold tracking-tight text-gray-900 dark:text-white">
+          Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport
+        </h5>
+      </a>
+      <div className="mb-1 mt-1 flex flex-row items-center">
+       
+       <span className="text-xl font-bold  dark:text-white pr-2 text-red-500"><del>$599</del></span>
+       <span className="text-xl font-bold text-green-900 dark:text-white">$400</span>
+      </div>
+      <div className="flex flex-col ">
+       
+        <a
+          href="#"
+          className="rounded-lg mb-1 bg-cyan-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-cyan-800 focus:outline-none focus:ring-4 focus:ring-cyan-300 dark:bg-cyan-600 dark:hover:bg-cyan-700 dark:focus:ring-cyan-800"
+        >
+          Add to cart
+        </a>
+        <a
+          href="#"
+          className="rounded-lg bg-pink-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-cyan-800 focus:outline-none focus:ring-4 focus:ring-cyan-300 dark:bg-cyan-600 dark:hover:bg-cyan-700 dark:focus:ring-cyan-800"
+        >
+        Buy Now
+        </a>
+      </div>
+    </Card>
+
+
+
+
+    <Card
+      className="max-w-sm"
+      imgAlt="Apple Watch Series 7 in colors pink, silver, and black"
+      imgSrc="/slider2/1.png"
+    >
+      <a href="#">
+        <h5 className="text-sm font-semibold tracking-tight text-gray-900 dark:text-white">
+          Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport
+        </h5>
+      </a>
+      <div className="mb-1 mt-1 flex flex-row items-center">
+       
+       <span className="text-xl font-bold  dark:text-white pr-2 text-red-500"><del>$599</del></span>
+       <span className="text-xl font-bold text-green-900 dark:text-white">$400</span>
+      </div>
+      <div className="flex flex-col ">
+       
+        <a
+          href="#"
+          className="rounded-lg mb-1 bg-cyan-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-cyan-800 focus:outline-none focus:ring-4 focus:ring-cyan-300 dark:bg-cyan-600 dark:hover:bg-cyan-700 dark:focus:ring-cyan-800"
+        >
+          Add to cart
+        </a>
+        <a
+          href="#"
+          className="rounded-lg bg-pink-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-cyan-800 focus:outline-none focus:ring-4 focus:ring-cyan-300 dark:bg-cyan-600 dark:hover:bg-cyan-700 dark:focus:ring-cyan-800"
+        >
+        Buy Now
+        </a>
+      </div>
+    </Card>
 
       
            </div>
